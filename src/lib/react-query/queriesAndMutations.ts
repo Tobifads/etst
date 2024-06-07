3f9e3a61eb23e45e1bc893e72289c327ae902b41
tobi fads <tobi.fadeyi.tf2@gmail.com>
2024-06-07 09:34:25 +0100

    initial commit

---
+++ src/lib/react-query/queriesAndMutations.ts
@@ -0,0 +1,27 @@
+// import{
+//     useQuery,
+//     useMutation,
+//     useQueryClient,
+//     useInfiniteQuery,
+// }from '@tanstack/react-query'
+
+import {
+    useMutation,
+} from '@tanstack/react-query';
+import { SigninAccount, createUserAccount } from '../appwirte/api'
+import { INewUser } from '@/types'
+
+export const useCreateUserAccount = () => {
+    return useMutation({
+        mutationFn: (user: INewUser) => createUserAccount(user)
+
+    })
+}
+export const useSigninAccount = () => {
+    return useMutation({
+        mutationFn: (user: {
+            email: string; password:
+            string}) => SigninAccount(user)
+
+    })
+}
